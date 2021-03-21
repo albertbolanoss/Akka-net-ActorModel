248c7772d3998a5575f82f44d31fe778886740ea
Luis.BolanosS Luis.BolanosS@perficient.com <Luis.BolanosS@perficient.com>
2021-03-20 21:30:30 -0500

    upload project

---
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,39 @@
+﻿using Akka.Actor;
+using MovieStreaming.Core;
+using System;
+
+namespace MovieStreaming.Actors
+{
+    class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Context.ActorOf(Props.Create<UserCordinatorActor>(), "UserCoordinator");
+            Context.ActorOf(Props.Create<PlaybackStatisticsActor>(), "PlaybackStatistics");
+        }
+
+        #region Hooks
+        protected override void PreStart()
+        {
+            ColorConsole.WriteLineGreen(string.Format("{0} PreStart", this.GetType().Name));
+        }
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineGreen(string.Format("{0} PostStop", this.GetType().Name));
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineGreen(string.Format("{0} PreRestart because {1}", this.GetType().Name, reason));
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineGreen(string.Format("{0} PostRestart because {1}", this.GetType().Name, reason));
+            base.PostRestart(reason);
+        }
+        #endregion
+    }
+}
---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using Akka.Actor;
+using MovieStreaming.Actors;
+using MovieStreaming.Core;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        static ActorSystem MovieStreamingActorSystem;
+
+        static void ShortPause()
+        {
+            Thread.Sleep(300);
+        }
+
+        static void Main(string[] args)
+        {
+            ColorConsole.WriteLineGray("Creating MovieStreamingActorSystem");
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            ColorConsole.WriteLineGray("Creating actor supervisor hierarchy");
+            MovieStreamingActorSystem.ActorOf(Props.Create<PlaybackActor>(), "Playback");
+
+            do
+            {
+                ShortPause();
+
+                Console.WriteLine();
+                ColorConsole.WriteLineDarkGray("Input a command and hit Enter");
+
+                string command = Console.ReadLine();
+                string[] split = command.Split(",");
+                string action = split[0];
+
+                ActorSelection userCoordinatorActor = MovieStreamingActorSystem.ActorSelection("/user/Playback/UserCoordinator");
+                ActorSelection playbackStatisticsActor = MovieStreamingActorSystem.ActorSelection("/user/Playback/PlaybackStatistics");
+
+                if (action.Equals("play"))
+                {
+                    int userId = int.Parse(split[1]);
+                    string movieTitle = split[2];
+                    var message = new PlayMovieMessage(movieTitle, userId);
+
+                    userCoordinatorActor.Tell(message);
+                    playbackStatisticsActor.Tell(message);
+                }
+
+                if (command.StartsWith("stop"))
+                {
+                    int userId = int.Parse(split[1]);
+                    var message = new StopMovieMessage(userId);
+
+                    userCoordinatorActor.Tell(message);
+                }
+
+                if (command.StartsWith("exit"))
+                {
+                    MovieStreamingActorSystem.Terminate();
+                    ColorConsole.WriteLineGray("Actor system shutdown");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+
+            } while (true);
+
+
+        }
+    }
+}
